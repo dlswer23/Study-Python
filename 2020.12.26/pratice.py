bdb4b58737b493a606a77c430a6f1d28a0f10e32
dlswer23 <lbj4298@naver.com>
2020-12-26 13:31:05 +0900

    [ADD] 2020.12.26

---
+++ 2020.12.26/pratice.py
@@ -0,0 +1,15 @@
+print(5)
+print(-10)
+print(3.14)
+print(5+3)
+print(2*8)
+print("풍선")
+
+# 문자열과 자료형을 곱할 수 있다.
+print("z"*7)
+
+# //참과 거짓
+# 만약 틀리다면 false가 출력
+print(5 > 10)
+
+print(not True)
---
+++ 2020.12.26/애완동물을 소개해주세요.py
@@ -0,0 +1,28 @@
+#만약에 연탄이를 해피로 바꾸어야 할 때가 생길 수 있다 .
+# 이럴때 변수를 사용하여야한다.
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby ="산책"
+
+#True와 false는 boolean 형태이다.
+
+is_adult= age >= 3
+
+print("우리집"+ animal+" 이름은"+name+"에요")
+print(name+ "는" +str(age)+"살이며 "+hobby+"을 아주 좋아해요")
+print(name+ "는 어른일까요? "+str(is_adult))
+
+
+animal = "고양이"
+name = "해피"
+age = 7
+hobby ="낮잠"
+
+#True와 false는 boolean 형태이다.
+
+is_adult= age >= 3
+
+print("우리집"+ animal+" 이름은"+name+"에요")
+print(name+ "는" +str(age)+"살이며 "+hobby+"을 아주 좋아해요")
+print(name+ "는 어른일까요? "+str(is_adult))
