fbf09f16246ceeb63e6ed4f2883b7e20ac560aae
dlswer23 <lbj4298@naver.com>
2021-01-07 23:27:19 +0900

    [ADD] 2021.1.7

---
+++ 2021.1.7/랜덤함수.py
@@ -0,0 +1,21 @@
+from random import*
+
+print(random())
+
+# 랜덤으로 숫자가 생성된다. 근데 범위는 0.0에서 1.0 이내로 생성된다.
+
+print(random()* 10)
+
+# 0.0부터 10.0까지 랜덤으로 가능하다.
+
+print(int(random()* 10))
+
+# 0부터 10까지 랜덤으로 가능하다.
+
+print(int(random()* 10)+1)
+
+# 1부터 10까지 랜덤으로 가능하다.
+
+print(randrange(1,46))
+
+#1부터 46까지 중에 랜덤으로 숫자가 나온다.
---
+++ 2021.1.7/숫자처리함수.py
@@ -0,0 +1,9 @@
+#from math import*
+
+print(ceil(4.99))
+#올림
+print(floor(2.33))
+#내림
+print(sqrt(16))
+#제곱근
+
